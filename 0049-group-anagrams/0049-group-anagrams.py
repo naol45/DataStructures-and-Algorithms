952f2d7e6336050c2e5116b1e1ae3795e89af215
Naol Weyessa <93046105+naol45@users.noreply.github.com>
2025-10-12 18:08:00 -0700

    Time: 28 ms (36.14%), Space: 18.1 MB (27%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        map = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            
+            map[tuple(count)].append(s)
+
+        return map.values()
